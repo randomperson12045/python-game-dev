609a53b4f4227ae43d1d161b45473d21470958b7
randomperson12045 <cantnapkai@gmail.com>
2026-01-13 16:30:46 +0800

    Add files via upload

---
+++ New folder/bumblebee.py
@@ -0,0 +1,42 @@
+import pgzrun
+from random import randint 
+
+
+TITLE="BEE"
+WIDTH=600
+HEIGHT=500
+SCORE=0
+
+bee=Actor('bee-removebg-preview')
+bee.pos=100,100
+flower=Actor('flower remove bg')
+flower.pos=200,200
+
+def draw():
+    screen.blit("background",(0,0))
+    bee.draw()
+    bee.scale=2
+    flower.draw()
+    screen.draw.text("SCORE: "+str(SCORE),color="black",topleft=(10,10))
+    
+def place_flower():
+    flower.x=randint(70,(WIDTH-70))
+    flower.y=randint(70,(HEIGHT-70))
+
+def update():
+    global SCORE
+    if keyboard.left:
+        bee.x-=2
+    if keyboard.right:
+        bee.x+=2
+    if keyboard.up:
+        bee.y-=2
+    if keyboard.down:
+        bee.y+=2
+    
+    flower_collected=bee.colliderect(flower)
+    if flower_collected:
+        SCORE+=10
+        place_flower()
+
+pgzrun.go()
