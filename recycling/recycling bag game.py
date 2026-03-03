f23e158efab317f2f65be1ac171af691c7fdbaaa
randomperson12045 <cantnapkai@gmail.com>
2026-03-03 16:16:41 +0800

    Add files via upload

--- recycling/recycling bag game.py
+++ recycling/recycling bag game.py
@@ -1,5 +1,5 @@
 import pgzrun
-from random import randint
+import random
 
 
 TITLE="CLICK THE BAG GAME"
@@ -9,12 +9,13 @@ centre_x=WIDTH/2
 centre_y=HEIGHT/2
 centre=(centre_x,centre_y)
 start_speed=10
-items=["paperbag","crate","plasticbag","bin"]
+ITEMS=["paperbag","crate","plasticbag","bin"]
 gameover=False
 game_complete=False
 current_level=1
-
-
+final_level=6
+items=[]
+animations=[]
 
 
 def draw():
@@ -29,9 +30,62 @@ def draw():
         for item in items:
             item.draw()
 
-    
-    
-    
+
+def update():
+    global items 
+    if len(items)==0:
+        items=make_items(current_level)
+        
+
+def make_items(number_of_extra_items):
+    items_to_create=get_option_to_create(number_of_extra_items)
+    new_items=create_items(items_to_create)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
+
+def get_option_to_create(number_of_extra_items):
+    items_to_create=["paperbag"]
+    for i in range(0,number_of_extra_items):
+        random_option=random.choice(ITEMS)
+        items_to_create.append(random_option)
+    return items_to_create
+
+def create_items(items_to_create):
+    new_items=[]
+    for option in items_to_create:
+        item=Actor(option)
+        new_items.append(item)
+    return new_items
+
+def layout_items(items_to_layout):
+    number_of_gaps=len(items_to_layout)
+    gap_size=WIDTH/number_of_gaps
+    random.shuffle(items_to_layout)
+    for index,item in enumerate(items_to_layout):
+        newX_pos=(index+1)*gap_size
+        item.x=newX_pos
+
+
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration=start_speed-current_level
+        item.anchor=("center","bottom")
+        animations=animate(item,duration=duration,on_finished=handle_game_over,y=HEIGHT)
+        animations.append(animations)
+
+def handle_game_over():
+    global gameover
+    gameover=True
+
+
+
+
+
+
+
 
 
 pgzrun.go()
